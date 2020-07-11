4518195cc89dd1a43f10f914e8b7e63fec3065ca
Brad-- <brad.smith.1324@gmail.com>
2020-07-11 12:50:27 -0600

    initial commit

---
+++ PatchController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatchController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Tick() {
+        Debug.Log("how many plots: " + this.GetPlots());
+    }
+
+    public void OnMouseDown() {
+        this.Tick();
+    }
+
+    public Component[] GetPlots() {
+        // Should this save to a local var?
+        return GetComponentsInChildren<PlotController>();
+    }
+}
---
+++ PlotController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotController : MonoBehaviour
+{
+
+    public string name = "Owen";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
